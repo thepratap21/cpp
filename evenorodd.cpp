05115fe33a05bf179ed3ecc7d2c5a8d155a9c476
Abhay Pratap <thepratapabhay21@gmail.com>
2023-08-01 21:42:23 +0530

    evenorodd.cpp

---
+++ evenorodd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter a number";
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"The number is even"<<endl;
+    }
+    else
+    cout<<"It is odd number";
+    return 0;
+}
